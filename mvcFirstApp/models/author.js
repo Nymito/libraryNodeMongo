102c3c8f48dc688967e99f4384b7919cb79dc802
Nymito <gianni.jouve@gmail.com>
2022-01-08 23:11:37 +0100

    Finished all routes  for author

--- mvcFirstApp/models/author.js
+++ mvcFirstApp/models/author.js
@@ -1,10 +1,24 @@
 const mongoose = require('mongoose')
-
+const Moovie = require('./moovie')
 const authorSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
     }
+})
+
+authorSchema.pre('remove', function(next) {
+    Moovie.find({author:this.id}, (err, moovies) =>{
+        if(err){
+            // cant connect to DB
+            next(err)
+        }else if(moovies.length>0){
+            // Author has moovies, dont remove
+            next(new Error('This author has moovies here'))
+        }else{
+            next()
+        }
+    })
 })
 
 module.exports = mongoose.model('Author', authorSchema)
